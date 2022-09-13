978bb6834466c4c8436338dd051fa454b32b5d7f
vigneshrml <vickysathy22@gmail.com>
2022-09-13 11:06:57 +0530

    v1 in dashboard

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,14 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
-import task from "@/store/modules/task.store"
+import task from "@/store/modules/task.store";
+import user from "@/store/modules/user.store";
 
 Vue.use(Vuex);
 
-
 export default new Vuex.Store({
-    modules : {
-     task
-    }
-  });
-  
+  modules: {
+    task,
+    user,
+  },
+});
