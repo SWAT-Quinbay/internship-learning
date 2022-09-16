4e91ea75b43a1ddf778caf4e2494555a2af4bab6
vigneshrml <vickysathy22@gmail.com>
2022-09-16 12:18:19 +0530

    employee role

--- src/service/employee.service.js
+++ src/service/employee.service.js
@@ -1,5 +1,8 @@
 import axios from "axios";
-import { BASE_EMPLOYEE_URL } from "@/constants/url.constant.js";
+import {
+  BASE_EMPLOYEE_URL,
+  BASE_TRAINGING_URL,
+} from "@/constants/url.constant.js";
 
 export const getEmployeeList = ({ success, error }) => {
   axios
@@ -12,11 +15,39 @@ export const getEmployeeList = ({ success, error }) => {
     });
 };
 
-
-export const getEmployeeDetails = ({ success, error ,employeeId}) => {
-  console.log(employeeId)
+export const getEmployeeDetails = ({ success, error, employeeId }) => {
+  console.log(employeeId);
   axios
     .get(`${BASE_EMPLOYEE_URL}/user/get/details/${employeeId}`)
+    .then((res) => {
+      success && success(res);
+    })
+    .catch((err) => {
+      error && error(err);
+    });
+};
+
+export const assignTrainingToEmployee = ({
+  success,
+  error,
+  employeeId,
+  trainingId,
+}) => {
+  console.log("trainingId", trainingId);
+  console.log("employeeId", employeeId);
+  axios
+    .post(`${BASE_TRAINGING_URL}/assign/${employeeId}/${trainingId}`)
+    .then((res) => {
+      success && success(res);
+    })
+    .catch((err) => {
+      error && error(err);
+    });
+};
+
+export const getTrainingByEmployeeId = ({ success, error, employeeId }) => {
+  axios
+    .get(`${BASE_TRAINGING_URL}/user-training/${employeeId}`)
     .then((res) => {
       success && success(res);
     })
--- src/views/Employee/EmployeeDashboard.vue
+++ src/views/Employee/EmployeeDashboard.vue
@@ -18,7 +18,7 @@
     <div class="row">
       <div
         class="col-md-3"
-        v-for="trainingdata in traininglist"
+        v-for="trainingdata in myTrainingList"
         :key="trainingdata.id"
       >
         <!-- {{ trainingdata}} -->
@@ -33,7 +33,7 @@ import TrainingCard from "@/components/TrainingCard.vue";
 import TextInputComponent from "@/components/TextInputComponent.vue";
 import ButtonComponent from "@/components/ButtonComponent.vue";
 import { mapGetters } from "vuex";
-import { getTokenRole } from "@/utils/storage.js";
+import { getTokenRole, getTokenUserId } from "@/utils/storage.js";
 export default {
   name: "EmployeeDashboard",
   data() {
@@ -54,11 +54,11 @@ export default {
   },
   computed: {
     ...mapGetters({
-      traininglist: "getTrainingList",
+      myTrainingList: "getMyTrainingList",
     }),
   },
   mounted() {
-    this.$store.dispatch("GET_TRAINING_LIST");
+    this.$store.dispatch("GET_TRAININGS_BY_EMPLOYEE_ID", getTokenUserId());
   },
   created() {
     console.log(getTokenRole());
